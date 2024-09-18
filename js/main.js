4ebb7b1eef87e1a521f8869c051647cdbdd906d2
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-09-18 15:58:37 -0600

    integracion de los archivos CSS (header, main, modal, projects, skills) el main.js y las imagenes .png y .svg

--- js/main.js
+++ js/main.js
@@ -18,3 +18,49 @@ const typed = new Typed('.typed', {
 	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
+
+// MODAL
+function openModal(project) {
+	const modal = document.getElementById('modal');
+	const modalTitle = document.getElementById('modal-title');
+	const modalDescription = document.getElementById('modal-description');
+	const modalImage = document.getElementById('modal-image');
+	
+	if (project === 'app') {
+		modalTitle.style.color = 'var(--blue-bg)'
+		modalTitle.style.marginBottom = '15px'
+	  modalTitle.textContent = 'Condominium Control';
+	  modalDescription.style.color = 'var(--text)'
+	  modalDescription.textContent = 'This application was developed to optimize and organize the data of the residents of a condominium. Initially, its focus was limited to a single condominium, but it expanded to be a generalizable tool for multiple condominiums. Its main functionalities include: a cloud database, user authentication through email and Google, CRUD operations with resident and condominium data, and data migration by reading QR codes. Currently, I am working on a new functionality that will allow the export of data through a numerical code, avoiding the need to have both devices nearby, as occurs with QR codes. The application uses HTML, CSS and JavaScript for the frontend, Firebase for the backend and version control with Git.';
+	  modalImage.src = '/img/app\ resident.png';
+	} else if (project === 'dashboard') {
+		modalTitle.style.color = 'var(--blue-bg)'
+		modalTitle.style.marginBottom = '15px'
+	  modalTitle.textContent = 'Dashboard Project';
+	  modalDescription.style.color = 'var(--text)'
+	  modalDescription.textContent = 'This dashboard was initially created as a practice for positioning elements with CSS. Later, interactive features were added using JavaScript, such as dark mode and a TradingView widget that shows the shares of relevant companies in the market. It was also integrated with the Binance API through WebSocket to display the price and volume of the most important cryptocurrencies in real time. In addition, the API of the "miindicador.cl" site was implemented to obtain key economic data from Chile, such as the value of the dollar, euro, UF, UTM, and IMACEC.';
+	  modalImage.src = '/img/dashboard.png';
+	} else if (project === 'samel') {
+		modalTitle.style.color = 'var(--blue-bg)'
+		modalTitle.style.marginBottom = '15px'
+	  modalTitle.textContent = 'Samel Castillo';
+	  modalDescription.style.color = 'var(--text)'
+	  modalDescription.textContent = 'This website was designed for Samel Castillo, a graphic artist and tattoo artist who was looking to display his work digitally to reach more clients. The site, which has a dark mode design, includes sections about his career, his portfolio of work, an image gallery, and a contact form for scheduling appointments. In addition, it has a testimonials section where customers can leave comments about their experience. An administration panel was also developed for the artist to manage their content, including the option to upload or delete images from the gallery and moderate comments.';
+	  modalImage.src = '/img/samel.png';
+	} else if (project === 'mareo') {
+		modalTitle.style.color = 'var(--blue-bg)'
+		modalTitle.style.marginBottom = '15px'
+	  modalTitle.textContent = 'Mareo Music';
+	  modalDescription.style.color = 'var(--text)'
+	  modalDescription.textContent = 'This website was designed for the musical group Mareo, recognized for its fresh and elegant style. The site highlights both the band and its individual members, with sections dedicated to their music, information about the band, live performances, and a contact page. It includes songs, new releases, upcoming performances, links to social networks, and a form to contact your manager directly. The site was developed with HTML, CSS, and JavaScript, using the Scroll Reveal library to add animations that make the content more dynamic.';
+	  modalImage.src = '/img/mareo.png';
+	}
+  
+	modal.style.display = 'flex';
+  }
+  
+  function closeModal() {
+	const modal = document.getElementById('modal');
+	modal.style.display = 'none';
+  }
+  
