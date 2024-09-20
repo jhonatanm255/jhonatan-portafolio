1ac2be8c349d8ff8aefdedf0868f1fcbec8a2e1d
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-09-20 17:37:50 -0600

    funcion de descarga de cv

---
+++ js/firebaseConfig.js
@@ -0,0 +1,17 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBa56KOB4BrhTunH_hF8CYnqgIqqEYiSsw",
+    authDomain: "mi-portafolio-44066.firebaseapp.com",
+    databaseURL: "https://mi-portafolio-44066-default-rtdb.firebaseio.com",
+    projectId: "mi-portafolio-44066",
+    storageBucket: "mi-portafolio-44066.appspot.com",
+    messagingSenderId: "992298539177",
+    appId: "1:992298539177:web:019db51f8660dd0c3631b8",
+    measurementId: "G-HFJRY7613P"
+  };
+
+  const btnModal = document.getElementById('send');
+  btnModal.addEventListener('click', () => {
+       console.log('hola')
+  })
